856fc72ad595b62240f9c147ffbc93da2ede6b85
39sho <g.39sho@gmail.com>
2024-06-28 11:44:34 +0900

    HoT: initial revision

---
+++ main.ts
@@ -0,0 +1,14 @@
+const toss = () => Math.floor(Math.random() * 2);
+
+const toss_n = 3; // トス回数
+
+console.log("Tossing a coin...");
+
+let sum = 0;
+for (const i of Array(toss_n).keys()) {
+    const n = toss();
+    sum += n;
+    console.log(`Round ${i + 1}: ${n === 0 ? "Heads" : "Tails"}`);
+}
+
+console.log(`Heads: ${toss_n - sum}, Tails: ${sum}`);
